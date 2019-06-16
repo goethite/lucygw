db7c14ef1b4920c1334df2fb535cc7e2990771d7
Graham Bevan <graham.bevan@ntlworld.com>
2019-06-16 20:10:45 +0100

    PoC ansible-k8s as a kafka consumer service, as opposed to a kubeless function.

--- init_main.sh
+++ init_main.sh
@@ -2,15 +2,14 @@
 
 GOVER="1.12.4"
 
+export DEBIAN_FRONTEND=noninteractive
 apt update
-apt install locales
+apt install -y locales python3-pip python3-dev virtualenv
 
 # Locales
 locale-gen en_GB
 locale-gen en_GB.UTF-8
 update-locale en_GB
-
-export DEBIAN_FRONTEND=noninteractive
 
 # Install Go
 wget -qO- https://dl.google.com/go/go${GOVER}.linux-amd64.tar.gz | \
